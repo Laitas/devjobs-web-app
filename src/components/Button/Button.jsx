9fbc2101dd1c7be16a92b8f66ac341c335a0f62a
Laitas <61273117+Laitas@users.noreply.github.com>
2021-09-04 10:23:40 +0300

    Implemented pagination

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import './Button.scss'
 
-const Button = ({text,type = 'one',mobileSearch = false}) => {
+const Button = ({text,type = 'one',mobileSearch = false,onClick}) => {
     if(mobileSearch){
-        return <button className={`btn mobile ${type}`}><AiOutlineSearch/></button>;
+        return <button onClick={onClick} className={`btn mobile ${type}`}><AiOutlineSearch/></button>;
     }
     return (
-        <button className={`btn ${type}`}>{text}</button>
+        <button onClick={onClick} className={`btn ${type}`}>{text}</button>
     )
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setJobs } from "../../redux/jobsSlice";
 import data from "../../data.json";
 import JobComponent from "../../components/JobComponent/JobComponent";
+import Button from '../../components/Button/Button'
 import './Homepage.scss'
 const Homepage = () => {
+  const [limit,setLimit] = useState(8)
   const dispatch = useDispatch();
   const jobs = useSelector((state) => state.jobs);
+  const loadMore = () =>{
+    if(limit > jobs.length){
+      setLimit(jobs.length)
+    }else{
+      setLimit(limit + 8)
+    }
+  }
   useEffect(() => {
     dispatch(setJobs(data));
     console.log(jobs);
-  }, []);
+  },[]);
   return (
     <section className="homepage">
-      {jobs.map(({ id, ...otherProps }) => (
+      <div className="jobs">
+      {jobs.slice(0,limit).map(({ id, ...otherProps }) => (
         <JobComponent key={id} {...otherProps} />
-      ))}
+        ))}
+        </div>
+      <div className="loadBtn">
+        <Button onClick={loadMore} text='Load more'/>
+      </div>
     </section>
   );
 };
